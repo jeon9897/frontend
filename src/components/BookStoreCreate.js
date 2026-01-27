c07c5d6aee380d10c22ddf1294b6ae16f36260c5
Jeon.c.w <33561661+jeon9897@users.noreply.github.com>
2026-01-27 17:58:09 +0900

    Update BookStoreCreate.js

--- src/components/BookStoreCreate.js
+++ src/components/BookStoreCreate.js
@@ -35,7 +35,7 @@ function BookStoreCreate(props) {
   //3. '신규상품 등록하기'버튼 클릭시 내용 백엔드로 주소전송하기
   const handleSubmit=(e)=>{
     e.preventDefault(); //새로고침 방지
-    axios.post('http://localhost:9070/bookstore', form)
+    axios.post('https://port-0-backend-express-server-mkvweaew5df78f72.sel3.cloudtype.app/bookstore', form)
     .then(()=>{ //성공시
       alert('상품이 등록 완료되었습니다.');
       // setBookstoreCount(count=>count+1);//숫자증가
@@ -154,5 +154,6 @@ function BookStoreCreate(props) {
     </main>
   );
 }
+
 
 export default BookStoreCreate;
