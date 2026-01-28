ade026a22ecf31c4fc9a20e46c09c2a0642ced4b
Jeon.c.w <33561661+jeon9897@users.noreply.github.com>
2026-01-28 15:54:41 +0900

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,8 @@ function Login(props) {
     //console.log(form.username, form.password)
 
     try{ //성공시 실행내용
-      const res = await axios.post('http://localhost:9070/login', form);
+      //const res = await axios.post('http://localhost:9070/login', form);
+      const res = await axios.post('https://port-0-backend-express-server-mkvweaew5df78f72.sel3.cloudtype.app/login', form);
       //사용자 인증이 끝나면 '토근'을 발급한다.
       localStorage.setItem('token', res.data.token);
       
@@ -122,5 +123,6 @@ function Login(props) {
     </main>
   );
 }
+
 
 export default Login;
